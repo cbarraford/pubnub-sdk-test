50165c7399fb571a90bf43a9ebec49cbfa9170e3
Chad Barraford <cbarraford@gmail.com>
2018-12-18 19:51:52 +0700

    add python support

---
+++ SDKs/python/test.py
@@ -0,0 +1,54 @@
+from pubnub.callbacks import SubscribeCallback
+from pubnub.enums import PNStatusCategory
+from pubnub.pnconfiguration import PNConfiguration
+from pubnub.pubnub import PubNub
+
+pnconfig = PNConfiguration()
+
+pnconfig.origin = 'pubnubcoin.com:4443'
+pnconfig.subscribe_key = 'demo'
+pnconfig.publish_key = 'demo'
+
+pubnub = PubNub(pnconfig)
+
+
+def my_publish_callback(envelope, status):
+    # Check whether request successfully completed or not
+    if not status.is_error():
+        pass  # Message successfully published to specified channel.
+    else:
+        pass  # Handle message publish error. Check 'category' property to find out possible issue
+        # because of which request did fail.
+        # Request can be resent using: [status retry];
+
+
+class MySubscribeCallback(SubscribeCallback):
+    def presence(self, pubnub, presence):
+        pass  # handle incoming presence data
+
+    def status(self, pubnub, status):
+        if status.category == PNStatusCategory.PNUnexpectedDisconnectCategory:
+            pass  # This event happens when radio / connectivity is lost
+
+        elif status.category == PNStatusCategory.PNConnectedCategory:
+            # Connect event. You can do stuff like publish, and know you'll get it.
+            # Or just use the connected event to confirm you are subscribed for
+            # UI / internal notifications, etc
+            print("ping")
+            pubnub.publish().channel("atari").message("pong").async(my_publish_callback)
+        elif status.category == PNStatusCategory.PNReconnectedCategory:
+            pass
+            # Happens as part of our regular operation. This event happens when
+            # radio / connectivity is lost, then regained.
+        elif status.category == PNStatusCategory.PNDecryptionErrorCategory:
+            pass
+            # Handle message decryption error. Probably client configured to
+            # encrypt messages and on live data feed it received plain text.
+
+    def message(self, pubnub, message):
+        print(message)
+        pass  # Handle new message stored in message.message
+
+
+pubnub.add_listener(MySubscribeCallback())
+pubnub.subscribe().channels('atari').execute()
